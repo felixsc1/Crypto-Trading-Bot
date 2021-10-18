726c6cb6efcb1c3dc68a38d2638ba8fcff1f7195
Felix Schlegel <felix.schlegel@alumni.ethz.ch>
2021-10-18 12:49:39 +0200

    error handling, private methods
    completed section3 (lesson17) binance part

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -45,43 +45,56 @@ logger = logging.getLogger()
 class BinanceFuturesClient:
     def __init__(self, public_key: str, secret_key: str, testnet: bool):
         if testnet:
-            self.base_url = "https://testnet.binancefuture.com"
-            self.wss_url = "wss://stream.binancefuture.com/ws"
+            self._base_url = "https://testnet.binancefuture.com"
+            self._wss_url = "wss://stream.binancefuture.com/ws"
         else:
-            self.base_url = "https://fapi.binance.com"
-            self.wss_url = "wss://fstream.binance.com/ws"
+            self._base_url = "https://fapi.binance.com"
+            self._wss_url = "wss://fstream.binance.com/ws"
 
-        self.public_key = public_key
-        self.secret_key = secret_key
+        self._public_key = public_key
+        self._secret_key = secret_key
 
-        self.headers = {'X-MBX-APIKEY': self.public_key}
+        self._headers = {'X-MBX-APIKEY': self._public_key}
 
         self.contracts = self.get_contracts()
         self.balances = self.get_balances()
 
         self.prices = dict()
 
-        self.id = 1
+        self._ws_id = 1
+        self._ws = None
 
         # since ws.run_forver() runs an infinite loop, everything below that line wouldn't execute
         # solution: run it in a thread, i.e. in a parallel background task.
-        t = threading.Thread(target=self.start_ws)
+        t = threading.Thread(target=self._start_ws)
         # self.start_ws() <-- alone wouldnt work
         t.start()
 
         logger.info("Binance Futures Client sucessfully initialized")
 
-    def generate_signature(self, data: dict) -> str: #check if generic dict works, maybe "typing.Dict" needed that allows more complex variables in it
+    def _generate_signature(self, data: dict) -> str: #check if generic dict works, maybe "typing.Dict" needed that allows more complex variables in it
         #encode() turns strings into byte-objects (as expected by hmac)
-        return hmac.new(self.secret_key.encode(), urlencode(data).encode(), hashlib.sha256).hexdigest()
+        return hmac.new(self._secret_key.encode(), urlencode(data).encode(), hashlib.sha256).hexdigest()
 
-    def make_request(self, method: str, endpoint: str, data: dict):
+    def _make_request(self, method: str, endpoint: str, data: dict):
         if method == "GET":
-            response = requests.get(self.base_url + endpoint, params=data, headers=self.headers)
+            try:
+                response = requests.get(self._base_url + endpoint, params=data, headers=self._headers)
+            except Exception as e:
+                logger.error(f"Connection error while making {method} request to {endpoint}: {e}")
+                return None  # because we won't even have a response.status_code
         elif method == "POST":
-            response = requests.post(self.base_url + endpoint, params=data, headers=self.headers)
+            try:
+                response = requests.post(self._base_url + endpoint, params=data, headers=self._headers)
+            except Exception as e:
+                logger.error(f"Connection error while making {method} request to {endpoint}: {e}")
+                return None
         elif method == "DELETE":
-            response = requests.delete(self.base_url + endpoint, params=data, headers=self.headers)
+            try:
+                response = requests.delete(self._base_url + endpoint, params=data, headers=self._headers)
+            except Exception as e:
+                logger.error(f"Connection error while making {method} request to {endpoint}: {e}")
+                return None
         else:
             raise ValueError()
 
@@ -93,7 +106,7 @@ class BinanceFuturesClient:
             return None
 
     def get_contracts(self) -> dict[str, Contract]:
-        exchange_info = self.make_request("GET", "/fapi/v1/exchangeInfo", dict())  # we send dict() empty dict, instead of None, because make_request() type hinting expects dict now.
+        exchange_info = self._make_request("GET", "/fapi/v1/exchangeInfo", dict())  # we send dict() empty dict, instead of None, because make_request() type hinting expects dict now.
 
         contracts = dict()
         if exchange_info is not None:
@@ -109,7 +122,7 @@ class BinanceFuturesClient:
         data['interval'] = interval
         data['limit'] = 1000
 
-        raw_candles = self.make_request("GET", "/fapi/v1/klines", data)
+        raw_candles = self._make_request("GET", "/fapi/v1/klines", data)
 
         candles = []
 
@@ -129,7 +142,7 @@ class BinanceFuturesClient:
         #"https://testnet.binancefuture.com/fapi/v1/ticker/bookTicker?symbol=BTCUSDT&key"
         data = dict()
         data['symbol'] = contract.symbol
-        ob_data = self.make_request("GET", "/fapi/v1/ticker/bookTicker", data)
+        ob_data = self._make_request("GET", "/fapi/v1/ticker/bookTicker", data)
 
         if ob_data is not None:
             bid_data = float(ob_data['bidPrice'])
@@ -146,12 +159,12 @@ class BinanceFuturesClient:
     def get_balances(self) -> dict[str, Balance]:
         data = dict()
         data['timestamp'] = int(time.time()*1000)
-        data['signature'] = self.generate_signature(data)
+        data['signature'] = self._generate_signature(data)
 
         balances = dict()
         # see https://binance-docs.github.io/apidocs/futures/en/#account-information-v2-user_data
         # for input (data is empty, except timestamp in signature) and response example
-        account_data = self.make_request("GET", "/fapi/v1/account", data)
+        account_data = self._make_request("GET", "/fapi/v1/account", data)
 
         if account_data is not None:
             for a in account_data['assets']:
@@ -174,9 +187,9 @@ class BinanceFuturesClient:
             data['timeInForce'] = tif
 
         data['timestamp'] = int(time.time()*1000)
-        data['signature'] = self.generate_signature(data)
+        data['signature'] = self._generate_signature(data)
 
-        order_status = self.make_request('POST', '/fapi/v1/order', data)
+        order_status = self._make_request('POST', '/fapi/v1/order', data)
         if order_status is not None:
             order_status = OrderStatus(order_status)
 
@@ -189,9 +202,9 @@ class BinanceFuturesClient:
         data['symbol'] = contract.symbol
 
         data['timestamp'] = int(time.time()*1000)
-        data['signature'] = self.generate_signature(data)
+        data['signature'] = self._generate_signature(data)
 
-        order_status = self.make_request('DELETE', '/fapi/v1/order', data)
+        order_status = self._make_request('DELETE', '/fapi/v1/order', data)
 
         if order_status is not None:
             order_status = OrderStatus(order_status)
@@ -204,32 +217,38 @@ class BinanceFuturesClient:
         data['timestamp'] = int(time.time() * 1000)
         data['symbol'] = contract.symbol
         data['orderId'] = order_id
-        data['signature'] = self.generate_signature(data)
+        data['signature'] = self._generate_signature(data)
 
-        order_status = self.make_request("GET", "/fapi/v1/order", data)
+        order_status = self._make_request("GET", "/fapi/v1/order", data)
 
         return order_status
 
 
-    def start_ws(self):
+    def _start_ws(self):
         # websocket connection using websocket-client module
-        self.ws = websocket.WebSocketApp(self.wss_url, on_open=self.on_open, on_close=self.on_close,
-        on_error=self.on_error, on_message=self.on_message)  # declared as instance variable, since we use it across many methods
-        self.ws.run_forever()  # check above under __init__ how to deal with this infinite loop problem in python
+        self._ws = websocket.WebSocketApp(self._wss_url, on_open=self._on_open, on_close=self._on_close,
+        on_error=self._on_error, on_message=self._on_message)  # declared as instance variable, since we use it across many methods
 
+        while True:  # By putting run_forever() in infinite while loop, it will restart if connection is interrupted.
+                     # Because any websocket error will trigger except statement, after which code continues
+            try:
+                self._ws.run_forever()  # check above under __init__ how to deal with this infinite loop problem in python
+            except Exception as e:
+                logger.error("Binance error in run_forever() method: {e}")
+            time.sleep(2) # if connection cannot be restarted immediately, prevents continuously trying
     # below functions that are called "callback functions"
-    def on_open(self):  #some versions may need "ws" as input argument here
+    def _on_open(self):  #some versions may need "ws" as input argument here
         logger.info("Binance connection opened")
 
-        self.subscribe_channel("BTCUSDT")
+        self.subscribe_channel(list(self.contracts.values()), 'bookTicker')  #not sure why its contracts.values here and not keys...
 
-    def on_close(self):
+    def _on_close(self):
         logger.warning("Binance websocket connection closed")
 
-    def on_error(self, msg: str):
+    def _on_error(self, msg: str):
         logger.error(f"Binance connection error: {msg}")
 
-    def on_message(self, msg: str):
+    def _on_message(self, msg: str):
         # print(msg)
         # example response is a dict:
         # {"e":"bookTicker","u":22437778066,"s":"BTCUSDT","b":"47229.85","B":"54.585","a":"47232.81","A":"65.202","T":1633092536099,"E":1633092536102}
@@ -255,19 +274,22 @@ class BinanceFuturesClient:
                 
                 # print(self.prices[symbol])
 
-    def subscribe_channel(self, contract: Contract):
+    def subscribe_channel(self, contracts: list[Contract], channel: str):
         #https://binance-docs.github.io/apidocs/spot/en/#live-subscribing-unsubscribing-to-streams
         # expects a json object, i.e. in python we create a dict with expected parameters
         data = dict()
         data['method'] = "SUBSCRIBE"
         data['params'] = []
-        data['params'].append(contract.symbol.lower() + "@bookTicker")  # e.g. channel "btcusdt@bookTicker" must be lowercase symbols
-        data['id'] = self.id
+
+        for contract in contracts:
+            data['params'].append(contract.symbol.lower() + "@" + channel)  # e.g. channel "btcusdt@bookTicker" must be lowercase symbols
+        data['id'] = self._ws_id
 
         # print(data, type(data))
         # print(json.dumps(data), type(json.dumps(data))) # looks the same, but dumps() turns it into json string
+        try:
+            self._ws.send(json.dumps(data)) #send method expects string, not dict
+        except Exception as e:
+            logger.error(f"Websocket error while subscribing to {len(contracts)} {channel} updates: {e}")
 
-        self.ws.send(json.dumps(data)) #send method expects string, not dict
-
-
-        self.id += 1  # instance variable from init, increase for every subscription
+        self._ws_id += 1  # instance variable from init, increase for every subscription
